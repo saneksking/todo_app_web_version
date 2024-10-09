ed83b783ec17854e523b183b83f084e9b5e2a525
A.A. Suvorov <saneksking@gmail.com>
2024-10-09 15:36:38 +0700

    Add class ToDo (NEED MIGRATE!!!)

--- persons/models.py
+++ persons/models.py
@@ -57,3 +57,15 @@ class Person(AbstractBaseUser, PermissionsMixin):
 
     def __str__(self):
         return self.email
+
+
+class ToDo(models.Model):
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, related_name='todos')
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    status = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
