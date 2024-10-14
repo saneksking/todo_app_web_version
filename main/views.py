1300f5cd39427fe1cae19b35c4ca1c688f8741b6
A.A. Suvorov <saneksking@gmail.com>
2024-10-14 18:44:14 +0700

    Style refactoring...

--- main/views.py
+++ main/views.py
@@ -1,10 +1,6 @@
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-from persons.models import Person
 
-
-@login_required
 def home(request):
     message = request.session.pop('message', None)
     context = {
