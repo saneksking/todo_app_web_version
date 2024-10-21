5cd4e73cd56ee3d5c676582ad45698a106f7043f
A.A. Suvorov <saneksking@gmail.com>
2024-10-21 19:02:30 +0700

    Update admin_panel

--- admin_panel/views.py
+++ admin_panel/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+from persons.models import Person
 
 
 def admin_index(request):
+    person_list = Person.objects.all()
     context = {
-
+        'person_list': person_list,
     }
     return render(request, 'admin_panel/admin_index.html', context)
